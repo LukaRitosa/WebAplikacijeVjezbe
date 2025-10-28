c34a9c2960ab9f9279a26818f5b492cfa8ef1220
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-10-28 09:31:12 +0100

    commit

--- samostalni_zadatak_za_vjezbu1/index.js
+++ samostalni_zadatak_za_vjezbu1/index.js
@@ -1,4 +1,5 @@
 const express= require("express")
+const path=require("path")
 
 const app= express()
 
@@ -6,8 +7,32 @@ const PORT= 3000
 
 app.get("/", (req, res)=>{
     console.log('pozvan je GET endpoint')
-    res.send("Pozdrav iz Express.js ")
+    res.sendFile(__dirname + "/public/index.html")
 })
+
+app.get("/about", (req, res)=>{
+    console.log('pozvan je GET endpoint')
+    res.sendFile(__dirname + "/public/about.html")
+})
+
+
+app.get("/users", (req, res)=>{
+    
+    const users=[
+        {
+            id: 0, ime:"Luka", prezime: "Ritoša"
+        },
+        {
+            id: 1, ime:"Ivo", prezime: "Ivić"
+        },
+        {
+            id: 2, ime:"Marko", prezime: "Markić"
+        }
+    ]
+    res.json(users)
+})
+
+
 
 app.listen(PORT, (error)=>{
     if(error){
