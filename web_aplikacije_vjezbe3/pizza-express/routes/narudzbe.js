125b2bcb3c977eab602bf0460c299bd3c213840f
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2025-12-23 18:44:32 +0100

    commit

--- web_aplikacije_vjezbe3/pizza-express/routes/narudzbe.js
+++ web_aplikacije_vjezbe3/pizza-express/routes/narudzbe.js
@@ -2,41 +2,136 @@
 
 import express from 'express';
 import { narudzbe, pizze } from '../data/data.js'; // učitavanje dummy podataka
+import { connectToDatabase } from '../db.js'
+import { FindCursor } from 'mongodb';
+import { ObjectId } from 'mongodb';
+
 const router = express.Router();
 
-// POST /narudzbe - Izrada nove narudžbe pizza
-router.post('/', (req, res) => {
-    const { narucene_pizze, podaci_dostava } = req.body;
-    
-    console.log('Primljeni podaci narudžbe:', req.body)
 
-    if (!narucene_pizze || narucene_pizze.length === 0) {
-        return res.status(400).json({ message: 'Nisu specificirane naručene pizze.' });
-    }
-    // Izračun ukupne cijene narudžbe
-    let ukupna_cijena = 0;
-    for (const narucena of narucene_pizze) {
-        const pizza = pizze.find(p => p.naziv.toLowerCase() === narucena.naziv.toLowerCase());
-        if (!pizza) {
-            return res.status(400).json({ message: `Pizza s nazivom '${narucena.naziv}' nije dostupna.` });
-        }
-        const cijena = pizza.cijene[narucena.velicina.toLowerCase()];
-        if (!cijena) {
-            return res.status(400).json({ message: `Veličina '${narucena.velicina}' nije dostupna za pizzu '${narucena.naziv}'.` });
-        }
-        ukupna_cijena += cijena * narucena.kolicina;
-    }
-    ukupna_cijena = Number(ukupna_cijena.toFixed(2)); // zaokruživanje ukupne cijene na 2 decimale
+
+let db = await connectToDatabase();
+
+// router.post('/', (req, res) => {
+//     const { narucene_pizze, podaci_dostava } = req.body;
+    
+//     console.log('Primljeni podaci narudžbe:', req.body)
+
+//     if (!narucene_pizze || narucene_pizze.length === 0) {
+//         return res.status(400).json({ message: 'Nisu specificirane naručene pizze.' });
+//     }
+//     // Izračun ukupne cijene narudžbe
+//     let ukupna_cijena = 0;
+//     for (const narucena of narucene_pizze) {
+//         const pizza = pizze.find(p => p.naziv.toLowerCase() === narucena.naziv.toLowerCase());
+//         if (!pizza) {
+//             return res.status(400).json({ message: `Pizza s nazivom '${narucena.naziv}' nije dostupna.` });
+//         }
+//         const cijena = pizza.cijene[narucena.velicina.toLowerCase()];
+//         if (!cijena) {
+//             return res.status(400).json({ message: `Veličina '${narucena.velicina}' nije dostupna za pizzu '${narucena.naziv}'.` });
+//         }
+//         ukupna_cijena += cijena * narucena.kolicina;
+//     }
+//     ukupna_cijena = Number(ukupna_cijena.toFixed(2)); // zaokruživanje ukupne cijene na 2 decimale
     
 
-    const nova_narudzba = {
-        id: narudzbe.length + 1,
-        narucene_pizze,
-        ukupna_cijena,
-        podaci_dostava
-    };
-    narudzbe.push(nova_narudzba);
-    res.status(201).json({ message: 'Narudžba je uspješno kreirana.', narudzba: nova_narudzba });
-    });
+//     const nova_narudzba = {
+//         id: narudzbe.length + 1,
+//         narucene_pizze,
+//         ukupna_cijena,
+//         podaci_dostava
+//     };
+//     narudzbe.push(nova_narudzba);
+//     res.status(201).json({ message: 'Narudžba je uspješno kreirana.', narudzba: nova_narudzba });
+//     });
+
+
+router.post('/', async (req, res)=>{
+    let narudzbe_collection = db.collection('narudzbe')
+    const nova_narudzba=req.body
+
+    let obavezniKljucevi = ['podaci_dostava', 'ukupna_cijena', 'narucene_pizze']
+
+    let obavezniKljuceviStavke = ['naziv', 'kolicina', 'velicina']
+    
+    let obavezniKljuceviPodatci = ['prezime', 'adresa', 'telefon']
+
+    
+
+    if (!obavezniKljucevi.every(kljuc => kljuc in nova_narudzba)) {
+        return res.status(400).json({ error: 'Nedostaju obavezni ključevi' });
+    }
+
+    if (!nova_narudzba.narucene_pizze.every(stavka => obavezniKljuceviStavke.every(kljuc => kljuc in stavka))) {
+        return res.status(400).json({ error: 'Nedostaju obavezni ključevi u stavci narudžbe' });
+    }
+
+    if (!obavezniKljuceviPodatci.every(kljuc => kljuc in nova_narudzba.podaci_dostava)){
+        return res.status(400).json({error: 'Nedostaju obavezni podaci za dostavu'})
+    }
+
+    if (!nova_narudzba.narucene_pizze.every(stavka => {
+            return Number.isInteger(stavka.kolicina) && stavka.kolicina > 0 && ['mala', 'srednja', 'jumbo'].includes(stavka.velicina)
+        })) {
+        return res.status(400).json({ error: 'Neispravni podaci u stavci narudžbe' });
+    }
+
+    let pizze_collection= db.collection('pizze')
+
+    let dostupne_pizze= await pizze_collection.find().toArray()
+
+    if(!nova_narudzba.narucene_pizze.every(stavka=> dostupne_pizze.some(pizza=> pizza.naziv==stavka.naziv))){
+        return res.status(400).json({greska: 'Odabrali ste pizzu koje nema u ponudi'})
+    }
+
+    let result={}
+    
+    try{
+        result= await narudzbe_collection.insertOne(nova_narudzba)
+        return res.status(201).json(result.insertedId)
+    } catch (error) {
+        console.log(error.errorResponse)
+        return res.status(400).json({error: error.errorResponse})
+    }
+})
+
+router.get('/mongo/narudzbe', async (req, res) => {
+    let narudzbe_collection = db.collection('narudzbe');
+    let narudzbe = await narudzbe_collection.find().toArray();
+
+    if (narudzbe.length === 0) {
+        return res.status(404).json({ error: 'Nema narudžbi' });
+    }
+
+    res.status(200).json(narudzbe);
+})
+
+router.get('/mongo/narudzbe/:id', async (req, res) => {
+    let narudzbe_collection = db.collection('narudzbe');
+    let id_param = req.params.id;
+    let narudzba = await narudzbe_collection.findOne({ _id: new ObjectId(id_param) }); // instanciramo objekt ObjectId
+
+    if (!narudzba) {
+        return res.status(404).json({ error: 'Narudžba nije pronađena' });
+    }
+
+    res.status(200).json(narudzba);
+})
+
+router.delete('/mongo/narudzbe/:naziv', async (req, res)=>{
+    let pizze_collection= db.collection('pizze')
+    let naziv_param= req.params.naziv
+
+    try{
+        let result= await pizze_collection.deleteOne({naziv: naziv_param})
+        return res.status(200).json({deletedCount: result.deletedCount})
+    } catch(error){
+        console.log(error.errorResponse)
+        return res.status(400).json({error: error.errorResponse})
+    }
+})
+
+
 
     export default router;
--- web_aplikacije_vjezbe3/pizza-express/routes/pizze.js
+++ web_aplikacije_vjezbe3/pizza-express/routes/pizze.js
@@ -2,29 +2,91 @@
 
 import express from 'express';
 import { pizze } from '../data/data.js'; // učitavanje dummy podataka
+import { connectToDatabase } from '../db.js'
+import { FindCursor } from 'mongodb';
+import { ObjectId } from 'mongodb';
+
 const router = express.Router();
 
 
-// GET /pizze - Dohvaćanje svih pizza (npr. GET /pizze)
-router.get('/', (req, res) => {
-    if (pizze.length === 0 || !pizze) {
-        return res.status(404).json({ message: 'Nema dostupnih pizza.' });
+// // GET /pizze - Dohvaćanje svih pizza (npr. GET /pizze)
+// router.get('/', (req, res) => {
+//     if (pizze.length === 0 || !pizze) {
+//         return res.status(404).json({ message: 'Nema dostupnih pizza.' });
+//     }
+
+//     res.status(200).json(pizze);
+// });
+
+// // GET /pizze/:naziv - Dohvaćanje pizze prema nazivu (npr. GET /pizze/Margherita)
+
+// router.get('/:naziv', (req, res) => {
+//     const naziv = req.params.naziv;
+//     const pizza = pizze.find(p => p.naziv.toLowerCase() === naziv.toLowerCase());
+
+//     if (!pizza) {
+//         return res.status(404).json({ message: `Pizza s nazivom '${naziv}' nije pronađena.` });
+//     }
+
+//     res.status(200).json(pizza);
+// });
+
+
+router.get('/', async (req, res)=>{
+    let pizze_collection= db.collection('pizze')
+    let pizze = await pizze_collection.find().toArray()
+    res.status(200).json(pizze) 
+})
+
+router.get('/:naziv_pizze', async (req, res)=>{
+    const naziv_pizze=req.params.naziv_pizze
+
+    let pizze_collection=db.collection('pizze')
+    let pizza= await pizze_collection.findOne({naziv: naziv_pizze})
+    res.status(200).json(pizza)
+})
+
+router.post('/', async (req, res)=>{
+    let pizze_collection = db.collection('pizze')
+    const nova_pizza=req.body
+
+
+    let obavezniKljucevi = ['naziv', 'sastojci', 'cijene', 'slika_url']
+
+    let obavezniKljuceviCijene = ['mala', 'srednja', 'jumbo']
+    
+    let dozvoljeniSastojci = ['rajčica', 'sir', 'gljive', 'bosiljak', 'paprika', 'šunka', 'feferoni ljuti', 'tunjevina', 'crveni luk', 'panceta', 'kulen', 'vrhnje']
+
+
+    if (!obavezniKljucevi.every(kljuc => kljuc in nova_pizza)) {
+        return res.status(400).json({ error: 'Nedostaju obavezni ključevi' });
     }
 
-    res.status(200).json(pizze);
-});
-
-// GET /pizze/:naziv - Dohvaćanje pizze prema nazivu (npr. GET /pizze/Margherita)
-
-router.get('/:naziv', (req, res) => {
-    const naziv = req.params.naziv;
-    const pizza = pizze.find(p => p.naziv.toLowerCase() === naziv.toLowerCase());
-
-    if (!pizza) {
-        return res.status(404).json({ message: `Pizza s nazivom '${naziv}' nije pronađena.` });
+    if (!nova_pizza.cijene.every(stavka => obavezniKljuceviCijene.every(kljuc => kljuc in stavka))) {
+        return res.status(400).json({ error: 'Nedostaju obavezni ključevi u stavci narudžbe' });
     }
 
-    res.status(200).json(pizza);
-});
+    if (!nova_pizza.sastojci.every(sastojak => {
+            return typeof sastojak=='string' && dozvoljeniSastojci.includes(sastojak)
+        })) {
+        return res.status(400).json({ error: 'Neispravni sastojci' });
+    }
+
+    if( typeof nova_narudzba.sastojci.mala !== 'number' || nova_narudzba.sastojci.mala < 0 ||
+        typeof nova_narudzba.sastojci.velika !== 'number' || nova_narudzba.sastojci.srednja < 0 ||
+        typeof nova_narudzba.sastojci.jumbo !== 'number' || nova_narudzba.sastojci.jumbo < 0){
+            return res.status(400).json({error: 'Neispravne cijene'})
+    }
+
+    let result={}
+    
+    try{
+        result= await pizze_collection.insertOne(nova_pizza)
+        return res.status(201).json(result.insertedId)
+    } catch (error) {
+        console.log(error.errorResponse)
+        return res.status(400).json({error: error.errorResponse})
+    }
+})
 
 export default router;
